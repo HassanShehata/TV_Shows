f0420f7f50f7262703bc2ca70e498ab889712a2c
HassanShehata <hassanshehata25895@gmail.com>
2018-06-03 08:16:33 +0200

    mbc-drama

--- program.py
+++ program.py
@@ -15,53 +15,41 @@ def file_handler(mode):
 
 	j=""
 	if mode == 'read':
-		f= open('channels.txt','r')
+		f= open('channels_programs.txt','r')
 		for line in f.readlines():
 			channels_pool.append(line)
 		f.close()
 
 	elif mode == 'write':
 		for i in results:
-			j=j+'      "'+i.split(":")[0]+'":"'+i.split(":")[1]+'",\n'
-		f= open('channels_data.json','w')
-		f.write("{\n \"live\":\n    [{\n"+j[:-2]+"\n   }]\n}")
+			j=j+i
+		f= open('program_data.json','w')
+		f.write("{\n \"channels-programs\":\n"+j[:-1]+"\n}")
 		
 
+def general(name,ch):
 
+	constract=""
+	if name.split('-')[0] == 'mbc':
+		browser.get(ch)
+		soup= bs(browser.page_source, "lxml")
+		page=soup.find_all("div",{"class":"teaser length-2halfhour"})
+		
+		for data in page:
+			#data_soup= bs(data,"lxml")
+			prog_name=data.find("h2").text
+			time=data.find("ul",{"class":"time time-2"}).text.strip("\n")
+			image="http://www.mbc.net"+data.find("img")['src']
+			constract=constract+("     \"prog_name\":\""+prog_name+"\"[{\n          \"time\":\""+time+"\",\n          \"image\":\""+image+"\"\n     }],")
 
-def fetch(url):
-	browser.get(url)
-	soup= bs(browser.page_source, "lxml")
-	return soup
-
-
-def general(name,ch,tag,info):
-
-	soup=fetch(ch)
-	flag=len(info.split(","))
-
-	if  flag == 2:
-		result=soup.find(tag,{info.split(",")[0]:info.split(",")[1]}).text
-		print(name+":"+result)
-		results.append(name+":"+result)
-
-	elif len(info) == 1:
-		result=soup.find_all(tag)[int(info)].text
-		print(name+":"+result)
-		results.append(name+":"+result)
-
-	"""
-	elif flag == 5:
-		result=soup.find(tag,{info.split(",")[0]:info.split(",")[1]}).text
-		result=soup.find(info.split(",")[2],{info.split(",")[3]:info.split(",")[4]}).text
-		print(name+":"+result)
-	"""
+		results.append("[{\n \""+name+"\":\n     [{\n"+constract[:-1]+"\n}],")
 
 
 def main():
+
 	file_handler("read")
 	for i in channels_pool:
-		general(i.split("*")[0],i.split("*")[1],i.split("*")[2],i.strip('\n').split("*")[3])
+		general(i.split("*")[0],i.strip('\n').split("*")[1])
 	file_handler("write")
 	
 
